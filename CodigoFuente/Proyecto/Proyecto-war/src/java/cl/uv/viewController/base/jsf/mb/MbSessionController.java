323dfbd55e9869ab16597edf8ea4982383be2701
Alejandro Alvarez <yano2h@gmail.com>
2012-06-11 13:01:07 -0400

    Estructura base del  proyecto

---
+++ CodigoFuente/Proyecto/Proyecto-war/src/java/cl/uv/viewController/base/jsf/mb/MbSessionController.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.uv.viewController.base.jsf.mb;
+
+import cl.uv.viewController.base.utils.Resources;
+import java.util.HashMap;
+import java.util.Map;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Alejandro
+ */
+@ManagedBean
+@SessionScoped
+public class MbSessionController {
+    public static final String  NAVIGATE_PATH = "pages";
+
+    private Map<String, String> pagesMap;
+    private String left = "";
+    private String top = "";
+    private String footer = "";
+    private String content = "";
+    
+    public MbSessionController() {
+        pagesMap = new HashMap<String, String>();
+        pagesMap.putAll(Resources.getMapPageList(MbSessionController.NAVIGATE_PATH));
+        this.top = pagesMap.get("header");
+        this.left = pagesMap.get("menuLeft");
+        this.footer = pagesMap.get("footer");
+        this.content = pagesMap.get("content");
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+
+    public void setFooter(String footer) {
+        this.footer = footer;
+    }
+
+    public String getLeft() {
+        return left;
+    }
+
+    public void setLeft(String left) {
+        this.left = left;
+    }
+
+    public String getTop() {
+        return top;
+    }
+
+    public void setTop(String top) {
+        this.top = top;
+    }
+    
+    
+}
