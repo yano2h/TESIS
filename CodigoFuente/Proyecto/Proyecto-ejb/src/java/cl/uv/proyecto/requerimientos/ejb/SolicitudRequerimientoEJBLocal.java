9a80a01c58ac0ce96e045463192a3aa89ea6dcc0
Alejandro <yano2h@gmail.com>
2012-07-08 01:02:27 -0400

    Se agrega nuevo ejb para las solicitudes requerimientos

---
+++ CodigoFuente/Proyecto/Proyecto-ejb/src/java/cl/uv/proyecto/requerimientos/ejb/SolicitudRequerimientoEJBLocal.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.uv.proyecto.requerimientos.ejb;
+
+import cl.uv.proyecto.persistencia.entidades.Funcionario;
+import cl.uv.proyecto.persistencia.entidades.SolicitudRequerimiento;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Jano
+ */
+@Local
+public interface SolicitudRequerimientoEJBLocal {
+    
+    String generarCodigoConsulta(SolicitudRequerimiento solicitud);
+
+    boolean validarCodigoConsulta(String codigoConsulta);
+
+    String enviarSolicitud(SolicitudRequerimiento solicitud, Funcionario solicitante);
+}
