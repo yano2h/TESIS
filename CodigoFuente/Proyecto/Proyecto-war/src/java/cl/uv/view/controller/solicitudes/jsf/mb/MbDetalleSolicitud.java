de6a93c096011db8beb510a549faed9297b93ca8
Alejandro <yano2h@gmail.com>
2012-07-11 02:33:29 -0400

    Se agrega la enviar comentario y listarlos

--- CodigoFuente/Proyecto/Proyecto-war/src/java/cl/uv/view/controller/solicitudes/jsf/mb/MbDetalleSolicitud.java
+++ CodigoFuente/Proyecto/Proyecto-war/src/java/cl/uv/view/controller/solicitudes/jsf/mb/MbDetalleSolicitud.java
@@ -6,14 +6,20 @@ package cl.uv.view.controller.solicitudes.jsf.mb;
 
 import cl.uv.proyecto.persistencia.ejb.ComentarioSolicitudFacadeLocal;
 import cl.uv.proyecto.persistencia.ejb.SolicitudRequerimientoFacadeLocal;
+import cl.uv.proyecto.persistencia.entidades.ComentarioSolicitud;
 import cl.uv.proyecto.persistencia.entidades.FuncionarioDisico;
 import cl.uv.proyecto.persistencia.entidades.SolicitudRequerimiento;
+import cl.uv.view.controller.base.jsf.mb.MbUserInfo;
 import cl.uv.view.controller.base.utils.JsfUtils;
 import java.io.Serializable;
+import java.util.Date;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
 
 /**
  *
@@ -28,14 +34,19 @@ public class MbDetalleSolicitud implements Serializable{
     @EJB
     private ComentarioSolicitudFacadeLocal comentarioFacade;
     
+    @ManagedProperty(value="#{mbUserInfo}")
+    private MbUserInfo mbUserInfo;
+    
     private String codigo;
+    private String comentario;
     private SolicitudRequerimiento solicitud;
     
+    
     public MbDetalleSolicitud() {
+        comentario="";
         codigo="";
     }
     
-  //  @PostConstruct
     public void init(){
         solicitud = solicitudFacade.buscarPorCodigo(codigo);
         FuncionarioDisico f = new FuncionarioDisico();
@@ -54,12 +65,37 @@ public class MbDetalleSolicitud implements Serializable{
         this.codigo = codigo;
     }
 
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
     public SolicitudRequerimiento getSolicitud() {
         return solicitud;
     }
 
     public void setSolicitud(SolicitudRequerimiento solicitud) {
         this.solicitud = solicitud;
+    }
+    
+    public void comentar(ActionEvent event){
+        if(!comentario.isEmpty()){
+            ComentarioSolicitud c = new ComentarioSolicitud();
+            c.setAutor(mbUserInfo.getFuncionario());
+            c.setFecha(new Date());
+            c.setVisible(true);
+            c.setComentario(comentario);
+            c.setSolicitudRequerimiento(solicitud);
+            comentarioFacade.create(c);
+        }
+        comentario="";
+    }
+
+    public void setMbUserInfo(MbUserInfo mbUserInfo) {
+        this.mbUserInfo = mbUserInfo;
     }
     
     
