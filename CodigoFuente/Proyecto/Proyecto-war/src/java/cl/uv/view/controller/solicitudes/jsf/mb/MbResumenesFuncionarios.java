32a0a03bf4c79a77a489f66b820807d5d4cb468a
Alejandro Alvarez <yano2h@gmail.com>
2012-07-17 15:00:21 -0400

    Inicio de pantalla de resumen

---
+++ CodigoFuente/Proyecto/Proyecto-war/src/java/cl/uv/view/controller/solicitudes/jsf/mb/MbResumenesFuncionarios.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.uv.view.controller.solicitudes.jsf.mb;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Alejandro
+ */
+@ManagedBean
+@ViewScoped
+public class MbResumenesFuncionarios {
+
+    /**
+     * Creates a new instance of MbResumenesFuncionarios
+     */
+    public MbResumenesFuncionarios() {
+    }
+}
