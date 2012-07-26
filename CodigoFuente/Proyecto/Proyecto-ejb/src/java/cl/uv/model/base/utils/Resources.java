4ed36dc6e921985bfd325e3156e40f33385f2a90
Alejandro Alvarez <yano2h@gmail.com>
2012-07-26 15:35:14 -0400

    Clase resources para EJB

---
+++ CodigoFuente/Proyecto/Proyecto-ejb/src/java/cl/uv/model/base/utils/Resources.java
@@ -0,0 +1,43 @@
+package cl.uv.view.controller.base.utils;
+
+import java.util.*;
+
+public class Resources {
+
+    public static final String PATH_RESOURCES = "cl.uv.view.controller.base.properties.Path";
+    
+    public static String getValue(String path, String key) {
+        return PropertyResourceBundle.getBundle(Resources.getPropertiesPath(path)).getString(key);
+    }
+
+    public static Short getValueShort(String path, String key) {
+        return Short.parseShort(getValue(path, key));
+    }
+    
+    public static Integer getValueInteger(String path, String key) {
+        return Integer.parseInt(getValue(path, key));
+    }
+    
+    public static Long getValueLong(String path, String key) {
+        return Long.parseLong(getValue(path, key));
+    }
+    
+    public static String getPropertiesPath(String key) {
+        return PropertyResourceBundle.getBundle(Resources.PATH_RESOURCES).getString(key);
+    }
+
+    public static ResourceBundle getPageList(String path) {
+        return PropertyResourceBundle.getBundle(Resources.getPropertiesPath(path));
+    }
+    
+    public static Map getMapPageList(String propertyName) {
+        ResourceBundle strBuffer = PropertyResourceBundle.getBundle(Resources.getPropertiesPath(propertyName));
+        Map mapPageList =  new HashMap();
+        for (Enumeration e = strBuffer.getKeys(); e.hasMoreElements();) {
+            String key = (String)e.nextElement();
+            mapPageList.put(key, strBuffer.getString(key));
+        }
+        
+        return mapPageList;
+    }
+}
