9120521922e8e0756f3528093f44d46a162ddda7
Alejandro Alvarez <yano2h@gmail.com>
2012-07-31 17:20:31 -0400

    se crean pantallas para misProyectos misTareas y crearProyecto

--- CodigoFuente/Proyecto/Proyecto-war/src/java/cl/uv/view/controller/proyecto/jsf/mb/MbMisProyectos.java
+++ CodigoFuente/Proyecto/Proyecto-war/src/java/cl/uv/view/controller/proyecto/jsf/mb/MbMisProyectos.java
@@ -5,7 +5,11 @@
 package cl.uv.view.controller.proyecto.jsf.mb;
 
 import cl.uv.proyecto.persistencia.ejb.ProyectoFacadeLocal;
+import cl.uv.proyecto.persistencia.entidades.Proyecto;
 import cl.uv.view.controller.base.jsf.mb.MbBase;
+import cl.uv.view.controller.base.utils.JsfUtils;
+import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -21,6 +25,30 @@ public class MbMisProyectos extends MbBase{
     @EJB
     private ProyectoFacadeLocal proyectoFacade;
             
+    private List<Proyecto> listaProyectos;
+    private Proyecto proyectoSelected;
+    
     public MbMisProyectos() {
+    }
+    
+    @PostConstruct
+    private void init(){
+        listaProyectos = proyectoFacade.buscarProyectosPorParticipante(getFuncionario());
+    }
+
+    public List<Proyecto> getListaProyectos() {
+        return listaProyectos;
+    }
+
+    public Proyecto getProyectoSelected() {
+        return proyectoSelected;
+    }
+
+    public void setProyectoSelected(Proyecto proyectoSelected) {
+        this.proyectoSelected = proyectoSelected;
+    }
+    
+    public void onRowSelect(){
+        JsfUtils.performNavigation("detalleProyecto?faces-redirect=true&idProyecto="+proyectoSelected.getIdProyecto(), false);
     }
 }
