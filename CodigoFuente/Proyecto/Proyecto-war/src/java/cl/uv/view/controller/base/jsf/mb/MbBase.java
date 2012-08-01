d1941615d1552e57079c92d251ce98514a1faedf
Alejandro Alvarez <yano2h@gmail.com>
2012-08-01 17:20:50 -0400

    se agrega el uso del contexto Flash

--- CodigoFuente/Proyecto/Proyecto-war/src/java/cl/uv/view/controller/base/jsf/mb/MbBase.java
+++ CodigoFuente/Proyecto/Proyecto-war/src/java/cl/uv/view/controller/base/jsf/mb/MbBase.java
@@ -7,11 +7,15 @@ package cl.uv.view.controller.base.jsf.mb;
 import cl.uv.proyecto.persistencia.entidades.FuncionarioDisico;
 import java.io.Serializable;
 import javax.faces.bean.ManagedProperty;
-
+import javax.faces.context.Flash;
+        
 public class MbBase implements Serializable{
     
     @ManagedProperty(value = "#{mbFuncionarioInfo}")
     private MbFuncionarioInfo mbFuncionarioInfo;
+    
+    @ManagedProperty("#{flash}")
+    private Flash flash;
     
     public MbBase(){}
     
@@ -21,6 +25,24 @@ public class MbBase implements Serializable{
 
     public MbFuncionarioInfo getMbFuncionarioInfo() {
         return mbFuncionarioInfo;
+    }
+
+    public Flash getFlash() {
+        return flash;
+    }
+
+    public void setFlash(Flash flash) {
+        this.flash = flash;
+    }
+    
+    public Object getValueOfFlashContext(String key){
+        Object v = getFlash().get(key);
+        getFlash().keep(key);
+        return v;
+    }
+            
+    public Object putValueOnFlashContext(String key, Object value){
+        return getFlash().put(key, value);
     }
     
     public FuncionarioDisico getFuncionario(){
