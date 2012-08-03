fb78cdf452cf7183dfb052735b4a967835b1983a
Alejandro Alvarez <yano2h@gmail.com>
2012-08-03 16:26:25 -0400

    inicio integracion con spring

---
+++ CodigoFuente/Proyecto/Proyecto-war/src/java/cl/uv/security/openam/OpenAMPreAuthenticatedProcessingFilter.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.uv.security.openam;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class OpenAMPreAuthenticatedProcessingFilter extends AbstractPreAuthenticatedProcessingFilter{
+    
+    private String cookieNameToken = null;//"iPlanetDirectoryPro";
+    
+    @Override
+    protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
+        return "N/A";
+    }
+
+    @Override
+    protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
+        return OpenAMUtil.getToken(cookieNameToken, request);
+    }
+
+    public String getCookieNameToken() {
+        return cookieNameToken;
+    }
+
+    public void setCookieNameToken(String cookieNameToken) {
+        this.cookieNameToken = cookieNameToken;
+    }
+    
+    
+}
