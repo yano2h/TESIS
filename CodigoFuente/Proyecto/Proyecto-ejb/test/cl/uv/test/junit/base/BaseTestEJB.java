d08e0b8cc5ec1e461cfa04b745108e4beca05956
Alejandro Alvarez <yano2h@gmail.com>
2012-08-27 11:55:41 -0400

    Configuracion basica para tests junit en EJB

---
+++ CodigoFuente/Proyecto/Proyecto-ejb/test/cl/uv/test/junit/base/BaseTestEJB.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.uv.test.junit.base;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.Context;
+import javax.naming.NamingException;
+import org.apache.commons.io.FileUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class BaseTestEJB {
+
+    private static Context ctx;  
+    private static EJBContainer container;
+    
+    private static final String TARGET_BUILD = "build/classes";
+    private static final String MODULE_NAME = "embedded";
+    private static final String TARGET_DIR = "build/" + MODULE_NAME;
+    private static final String PATH_RESOURCES_TEST = "test/resources/";
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        Map<String, Object> properties = new HashMap<String, Object>();
+        properties.put(EJBContainer.MODULES, prepareModuleDirectory());
+        properties.put("org.glassfish.ejb.embedded.glassfish.configuration.file", PATH_RESOURCES_TEST + "domain.xml");
+        container = javax.ejb.embeddable.EJBContainer.createEJBContainer(properties);
+        ctx = container.getContext();
+    }
+    
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        container.close();
+    }
+    
+    private static File prepareModuleDirectory() throws IOException {
+        File result = new File(TARGET_DIR);
+        FileUtils.copyDirectory(new File(TARGET_BUILD), result);
+        FileUtils.copyFile(new File(PATH_RESOURCES_TEST + "test-persistence.xml"),                
+                new File(TARGET_DIR + "/META-INF/persistence.xml"));
+        return result;
+    }
+
+    protected <T> T lookupBy(Class<T> type) throws NamingException {
+        return (T) ctx.lookup("java:global/" + MODULE_NAME + "/" + type.getSimpleName());
+    }
+}
