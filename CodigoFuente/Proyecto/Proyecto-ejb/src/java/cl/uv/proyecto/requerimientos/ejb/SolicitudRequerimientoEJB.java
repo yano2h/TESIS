13c2b862566b8274156cc13b37185e1872d770df
Alejandro Alvarez <yano2h@gmail.com>
2012-08-28 17:14:19 -0400

    correcion metodo generarCodigoConsulta

--- CodigoFuente/Proyecto/Proyecto-ejb/src/java/cl/uv/proyecto/requerimientos/ejb/SolicitudRequerimientoEJB.java
+++ CodigoFuente/Proyecto/Proyecto-ejb/src/java/cl/uv/proyecto/requerimientos/ejb/SolicitudRequerimientoEJB.java
@@ -12,6 +12,8 @@ import cl.uv.proyecto.persistencia.ejb.TipoPrioridadFacadeLocal;
 import cl.uv.proyecto.persistencia.entidades.*;
 import java.util.Date;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
@@ -21,14 +23,12 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class SolicitudRequerimientoEJB implements SolicitudRequerimientoEJBLocal {
-    
+
     private final long MOD = 1000L;
     private final long DESPLAZAMIENTO = 10000000000L;
     private final long TIME_INIT = 1341773584868L;
     private final short PRIORIDAD_INICIAL = 0;
     private final int CIERRE = 0;
-
-
     @EJB
     private SolicitudRequerimientoFacadeLocal solicitudFacade;
     @EJB
@@ -37,12 +37,12 @@ public class SolicitudRequerimientoEJB implements SolicitudRequerimientoEJBLocal
     private TipoPrioridadFacadeLocal tipoPrioridadFacade;
     @EJB
     private EmailEJBLocal emailEJB;
-    
+
     @Override
     public String generarCodigo(long num) {
         String code = "";
         String s = "1qQaAzZ2wWsSxX3eEdDcC4rRfFvV5TtgGbB6yYhHnN7uUjJmM8iIkK9oOlpP0";
-        int length = s.length()-1;
+        int length = s.length() - 1;
         while (num > 0) {
             int n = (int) (num % length);
             code += String.valueOf(s.charAt(n));
@@ -50,24 +50,26 @@ public class SolicitudRequerimientoEJB implements SolicitudRequerimientoEJBLocal
         }
         return code;
     }
-     
+
     @Override
-    public String generarCodigoConsulta(SolicitudRequerimiento solicitud) {      
-        String codigo = "";   
+    public String generarCodigoConsulta(SolicitudRequerimiento solicitud) {
+        String codigo = "";
         Random rand = new Random();
-        rand.setSeed(new Date().getTime());
-        
-        long base = ( rand.nextInt(8) + 1 ) * MOD;
-        long time = (solicitud.getFechaEnvio().getTime() - TIME_INIT)/MOD;
-        base += solicitud.getSolicitante().getRut()%MOD;
-        base = base*DESPLAZAMIENTO + time;
-        
-        return generarCodigo(base);
+        rand.setSeed(new Date().getTime() - solicitud.getSolicitante().getRut());
+        do {
+            long base = (rand.nextInt(8) + 1) * MOD;
+            long time = (solicitud.getFechaEnvio().getTime() - TIME_INIT) / MOD;
+            base += solicitud.getSolicitante().getRut() % MOD;
+            base = base * DESPLAZAMIENTO + time;
+            codigo = generarCodigo(base);
+        } while (!validarCodigoConsulta(codigo));
+
+        return codigo;
     }
 
     @Override
     public boolean validarCodigoConsulta(String codigoConsulta) {
-        return false;
+        return (solicitudFacade.buscarPorCodigo(codigoConsulta) == null);
     }
 
     @Override
@@ -84,96 +86,90 @@ public class SolicitudRequerimientoEJB implements SolicitudRequerimientoEJBLocal
     }
 
     @Override
-    public void rechazarSolicitud(SolicitudRequerimiento solicitud){
+    public void rechazarSolicitud(SolicitudRequerimiento solicitud) {
         EstadoSolicitudRequerimiento estado = estadoSolicitudFacade.find(EstadoSR.RECHAZADA);
         solicitud.setEstadoSolicitud(estado);
         solicitudFacade.edit(solicitud);
     }
-    
-    private void cerrarSolicitud(SolicitudRequerimiento solicitud){
+
+    private void cerrarSolicitud(SolicitudRequerimiento solicitud) {
         solicitud.setEstadoSolicitud(estadoSolicitudFacade.find(EstadoSR.CERRADA));
         solicitud.setFechaCierre(new Date());
         solicitudFacade.edit(solicitud);
     }
-    
+
     @Override
-    public void enviarRespuestaDirecta(SolicitudRequerimiento solicitud, Boolean enviarCopiaCorreo){
+    public void enviarRespuestaDirecta(SolicitudRequerimiento solicitud, Boolean enviarCopiaCorreo) {
         cerrarSolicitud(solicitud);
-        if(enviarCopiaCorreo){
+        if (enviarCopiaCorreo) {
             String email = solicitud.getSolicitante().getCorreoUv();
             String asunto = crearAsunto(solicitud, CIERRE);
             String mensaje = crearMensaje(solicitud, CIERRE);
             emailEJB.enviarEmail(email, asunto, mensaje);
         }
     }
-    
+
     @Override
-    public void enviarRespuestaManual(SolicitudRequerimiento solicitud, String[] direcciones, String asunto){
+    public void enviarRespuestaManual(SolicitudRequerimiento solicitud, String[] direcciones, String asunto) {
         cerrarSolicitud(solicitud);
-        asunto = (asunto.isEmpty())?crearAsunto(solicitud, CIERRE): asunto;
+        asunto = (asunto.isEmpty()) ? crearAsunto(solicitud, CIERRE) : asunto;
         String mensaje = crearMensaje(solicitud, CIERRE);
         emailEJB.enviarEmail(direcciones, asunto, mensaje);
-    
+
     }
-    
+
     @Override
-    public void transferirSolicitud(SolicitudRequerimiento solicitud, Area nuevaAreaResponsable, String motivoTransferencia){
+    public void transferirSolicitud(SolicitudRequerimiento solicitud, Area nuevaAreaResponsable, String motivoTransferencia) {
         solicitud.setJustificacionTrasnferencia(motivoTransferencia);
         solicitud.setAreaResponsable(nuevaAreaResponsable);
         solicitud.setEstadoSolicitud(estadoSolicitudFacade.find(EstadoSR.TRANSFERIDA));
         solicitudFacade.edit(solicitud);
     }
-    
+
     @Override
-    public void asignarSolicitud(SolicitudRequerimiento solicitud){
+    public void asignarSolicitud(SolicitudRequerimiento solicitud) {
         solicitud.setEstadoSolicitud(estadoSolicitudFacade.find(EstadoSR.ASIGNADA));
         solicitudFacade.edit(solicitud);
     }
-    
+
     @Override
-    public void iniciarSolicitud(SolicitudRequerimiento solicitud){
+    public void iniciarSolicitud(SolicitudRequerimiento solicitud) {
         solicitud.setEstadoSolicitud(estadoSolicitudFacade.find(EstadoSR.INICIADA));
         solicitudFacade.edit(solicitud);
     }
-    
+
     @Override
-    public void enviarRespuestaJefeArea(SolicitudRequerimiento solicitud){
+    public void enviarRespuestaJefeArea(SolicitudRequerimiento solicitud) {
         solicitud.setEstadoSolicitud(estadoSolicitudFacade.find(EstadoSR.FINALIZADA_SIN_RESPUESTA));
         solicitudFacade.edit(solicitud);
     }
-    
-    
-    private String crearMensaje(SolicitudRequerimiento solicitud, int tipoMensaje){
+
+    private String crearMensaje(SolicitudRequerimiento solicitud, int tipoMensaje) {
         String mensaje = "";
         switch (tipoMensaje) {
             case CIERRE:
-                mensaje = "<strong>[Codigo: "+solicitud.getCodigoConsulta()+" ]["+solicitud.getAsunto()+"]</strong>"+
-                          "<div><br/><strong>Respuesta:</strong><br/></div>"+
-                           solicitud.getRespuesta()+
-                           "<br/><div>Para mayor información ingrese al sistema</div>";
+                mensaje = "<strong>[Codigo: " + solicitud.getCodigoConsulta() + " ][" + solicitud.getAsunto() + "]</strong>"
+                        + "<div><br/><strong>Respuesta:</strong><br/></div>"
+                        + solicitud.getRespuesta()
+                        + "<br/><div>Para mayor información ingrese al sistema</div>";
                 break;
             default:
                 throw new AssertionError();
         }
-        
-       return mensaje;
+
+        return mensaje;
     }
-    
-    private String crearAsunto(SolicitudRequerimiento solicitud, int tipoAsunto){
+
+    private String crearAsunto(SolicitudRequerimiento solicitud, int tipoAsunto) {
         String asunto = "";
         switch (tipoAsunto) {
             case CIERRE:
-                asunto = "La solicitud "+solicitud.getCodigoConsulta()+" fue cerrada.";
+                asunto = "La solicitud " + solicitud.getCodigoConsulta() + " fue cerrada.";
                 break;
             default:
                 throw new AssertionError();
         }
-        
-       return asunto;
-    }
-    
-    
-    
-   
-}
 
+        return asunto;
+    }
+}
