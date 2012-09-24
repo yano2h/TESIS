7e8f44dc563fd77b822cb6606b4747d7b0ca32c7
Alejandro Alvarez <yano2h@gmail.com>
2012-09-24 17:04:23 -0400

    nueva bd en postgres

---
+++ CodigoFuente/datos_iniciales_postgres.sql
@@ -0,0 +1,132 @@
+INSERT INTO AREA (id_area, nombre_area, descripcion_area) VALUES 
+(1, 'Desarrollo', 'Área de Desarrollo de Sistemas de Información es la encargada del desarrollo de sistemas informáticos, que permitan mejorar la calidad de los servicios que se brindan a alumnos, profesores y funcionarios de la Universidad.'),
+(2, 'Fincon', 'Área de Sistemas Financiero-Contables es la encargada de automatizar los diferentes procesos que levan a cabo la Dirección de Administración y Finanzas, Dirección de Recursos Humanos y la Unidad de Aranceles de la Universidad, además de garantizar el funcionamiento de manera ininterrumpida y eficiente de los sistemas que apoyan estos procesos.'),
+(3, 'Redes y Soporte','Área de Redes, Telecomunicaciones y Soporte es la encargada del correcto funcionamiento de los servicios de redes y comunicaciones, además del soporte de hardware y software de los sistemas computacionales y usuarios de la universidad.');
+
+INSERT INTO ESTADO_SOLICITUD_REQ (id_estado_solicitud_req, nombre_estado_solicitud, descripcion_estado) VALUES 
+(0, 'Enviada', 'La solicitud ha sido enviada al area correspondiente y en espera de asignación.'),
+(1, 'Rechazada', 'La solicitud a sido rechazada.'),
+(2, 'Transferida a otra Àrea', 'La solicitud ha sido transferida a otra area.'),
+(3, 'Asignada', 'Se a asignado un funcionario quien debe atenderla y resolverla.'),
+(4, 'Pendiente', 'El funcionario a rrevisado la solicitud pero no ha comenzado a trabajar en ella.'),
+(5, 'Iniciada', 'El funcionario se encuentra trabajando en la solicitud.'),
+(6, 'Vencida', 'La solicitud a superado el plazo maximo establecido para resolverla.'),
+(7, 'Finalizada en espera de aprobación', 'La solicitud a sido completada y se a devuelto al jefe de area para que sea el quien envie una repsuesta oficial'),
+(8, 'Cerrada', 'La solicitud ya se ha resuelto por lo que ya no puede ser modificada');
+
+INSERT INTO ESTADO_SOLICITUD_CAMBIO (id_estado_solicitud_cambio, nombre_estado_solicitud) VALUES 
+(0, 'Enviada'),
+(1, 'Analisada'),
+(2, 'Aprobada'),
+(3, 'Desaprobada'),
+(4, 'Implementada');
+
+INSERT INTO TIPO_PRIORIDAD (id_tipo_prioridad, nombre_prioridad) VALUES
+(0, 'No Asignada'),
+(1, 'Baja'),
+(2, 'Media'),
+(3, 'Alta'),
+(4, 'Critica');
+
+INSERT INTO TIPO_SOLICITUD_REQ (id_tipo_solicitud_req, nombre_tipo_solicitud, descripcion_tipo) VALUES 
+(1, 'Correo UV', ''),
+(2, 'Clave', ''),
+(3, 'Cambio de Clave', ''),
+(4, 'Acceso a aplicación', ''),
+(5, 'Información', ''),
+(6, 'Actualización de Información', ''),
+(7, 'Reporte', ''),
+(8, 'Reporte SharePoint', ''),
+(9, 'Reparación de Conexión', ''),
+(10, 'Reparación de Equipo', '');
+
+INSERT INTO FUNCIONARIO (rut, nombre, apellido_paterno, apellido_materno, correo_uv, fecha_ultimo_acceso, fecha_primer_acceso) VALUES
+(16775578, 'Alejandro Andres', 'Alvarez', 'Ahumada', 'yano2h@gmail.com', NULL, NULL),
+(17010101, 'Alberto Felipe', 'Alvarez', 'Gonzales', 'alberto.felipe@uv.cl', NULL, NULL),
+(15409873, 'Carlos Gillermo', 'Villalon', 'Gonzales', 'felipe.villalon@uv.cl', NULL, NULL),
+(15409800, 'Andres Mauricio', 'Plaza', 'Rivera', 'andres.plaza@uv.cl', NULL, NULL),
+(18000000, 'Alejandro Andres', 'Alvarez', 'Ahumada', 'yano2h@gmail.com', '2012-07-18 16:53:56', '2012-07-12 16:42:20'),
+(19000000, 'Rodrigo', 'Cabrera', 'Muñoz', 'rodrigo.cabrera@uv.cl', NULL, NULL),
+(20000000, 'Sergio', 'Valdivia', 'Castro', 'sergio.valdivia@uv.cl', NULL, NULL);
+
+INSERT INTO FUNCIONARIO_DISICO (rut, area, cargo, anexo)VALUES 
+(16775578, 1, 'Ayudante', '0000'),
+(18000000, 1, 'Ayudante', '2145'),
+(19000000, 1, 'Analista/Desarrollo de Sistemas', '2546'),
+(20000000, 1, 'Analista/Desarrollo de Sistemas', '3569');
+
+
+INSERT INTO NOTIFICACION (fecha, mensaje_notificacion, revisada, destinatario)values
+(CURRENT_TIMESTAMP,'Notificacion 11', false,16775578),
+(CURRENT_TIMESTAMP,'Notificacion 12', false,16775578),
+(CURRENT_TIMESTAMP,'Notificacion 13', false,16775578),
+(CURRENT_TIMESTAMP,'Notificacion 14', false,16775578),
+(CURRENT_TIMESTAMP,'Notificacion 15', false,16775578),
+(CURRENT_TIMESTAMP,'Notificacion 16', false,16775578),
+(CURRENT_TIMESTAMP,'Notificacion 17', false,16775578),
+(CURRENT_TIMESTAMP,'Notificacion 18', false,16775578),
+(CURRENT_TIMESTAMP,'Notificacion 19', false,16775578),
+(CURRENT_TIMESTAMP,'Notificacion 20', false,16775578);
+
+INSERT INTO ESTADO_PROYECTO values
+(1, 'Activo'),
+(2, 'Espera HH disponible'),
+(3, 'Consulta'),
+(4, 'Detenido por Prioridad'),
+(5, 'Detenido por Cambio de contraparte'),
+(6, 'Detenido por Requerimiento'),
+(7, 'Espera Por revisión Unidad'),
+(8, 'Espera Reunión coordinación'),
+(9, 'Finalizado');
+
+INSERT INTO TIPO_PROYECTO values
+(1, 'Desarrollo'),
+(2, 'Investigacion'),
+(3, 'Migracion de Datos');
+
+INSERT INTO ROL_PROYECTO values
+(1, 'Jefe de Proyecto'),
+(2, 'Diseñador'),
+(3, 'Desarrollador'),
+(4, 'Tester');
+
+INSERT INTO TAREA_SCM values
+('1', 'Identificar y Nombrar Items de Configuracion', ''),
+('2', 'Solicitar un Cambio', ''),
+('3', 'Analizar Solicitudes de Cambio', ''),
+('4', 'Aprobar o Desaprobar las Solicitudes de Cambio', ''),
+('5', 'Implementar el Cambio', ''),
+('6', 'Actualizar Documentación en la Biblioteca de Software', ''),
+('7', 'Actualizar Codigo en la Biblioteca de Software', ''),
+('8', 'Completar Check-List', '');
+
+INSERT INTO ENTREGABLE values
+('1', 'Documento de Identificación de la Configuración', '1'),
+('2', 'Solicitud de Cambio', '2'),
+('3', 'Analisis de Solicitud de Cambio', '3'),
+('4', 'Documento del Usuario', '4'),
+('5', 'Documento de Implementacion de Cambios', '5'),
+('6', 'Actualización Biblioteca de Software', '6'),
+('7', 'Actualización Biblioteca de Software', '7'),
+('8', 'Check-List', '8');
+
+
+--Funcionarios para Test Junit
+INSERT INTO FUNCIONARIO (rut, nombre, apellido_paterno, apellido_materno, correo_uv, fecha_ultimo_acceso, fecha_primer_acceso) VALUES
+(11111111, 'Usuario Uno', 'Paterno', 'Materno', 'user.uno@uv.cl', NULL, NULL), 
+(22222222, 'Usuario Dos', 'Paterno', 'Materno', 'user.dos@uv.cl', NULL, NULL);
+
+--Funcionarios para Test Junit
+INSERT INTO FUNCIONARIO_DISICO (rut, area, cargo, anexo)VALUES 
+(11111111, 1, 'Developer', '0000'),
+(22222222, 1, 'Tester', '2145');
+
+--Solicitud que debe cambiarse a vencida para test
+INSERT INTO solicitud_requerimiento (id_solicitud_req, asunto, codigo_consulta, fecha_envio, fecha_ultima_actualizacion, fecha_vencimiento, mensaje, respuesta, area_responsable, estado_solicitud, prioridad_solicitud, responsable, solicitante, tipo_solicitud) 
+VALUES 
+(1, 'Enviada y Vencida', 'ABCD' ,'2012-07-09 09:51:23', '2012-07-09 09:51:23', '2012-07-19 09:51:23', 'Mensaje', 'Respuesta', 1, 0, 0, NULL, 11111111, 1),
+(2, 'Vencida', 'EFGH', '2012-07-09 09:51:23', '2012-07-09 09:51:23', '2012-07-19 09:51:23', 'Mensaje', 'Respuesta', 1, 6, 0, 22222222, 11111111, 1),
+(3, 'Finalizada sin respuesta', 'IJKL', '2012-07-09 09:51:23', '2012-07-09 09:51:23', '2012-07-19 09:51:23', 'Mensaje', 'Respuesta', 1, 7, 0, 22222222, 11111111, 1),
+(4, 'Cerrada', 'MNLP', '2012-07-09 09:51:23', '2012-07-09 09:51:23', '2012-07-19 09:51:23', 'Mensaje', 'Respuesta', 1, 8, 0, 22222222, 11111111, 1),
+(5, 'Sin fecha vencimiento', 'QRST', '2012-07-09 09:51:23', '2012-07-09 09:51:23', NULL, 'Mensaje', 'Respuesta', 1, 1, 0, 22222222, 11111111, 1);
+
---
+++ CodigoFuente/script_bd_postgres_v1.sql
@@ -0,0 +1,477 @@
+
+-- -----------------------------------------------------
+-- Table TIPO_SOLICITUD_REQ
+-- -----------------------------------------------------
+CREATE TABLE TIPO_SOLICITUD_REQ (
+  id_tipo_solicitud_req SMALLINT NOT NULL ,
+  nombre_tipo_solicitud VARCHAR(45) NOT NULL ,
+  descripcion_tipo VARCHAR(255) NULL ,
+  PRIMARY KEY (id_tipo_solicitud_req) );
+
+
+-- -----------------------------------------------------
+-- Table ESTADO_SOLICITUD_REQ
+-- -----------------------------------------------------
+CREATE TABLE ESTADO_SOLICITUD_REQ (
+  id_estado_solicitud_req SMALLINT NOT NULL ,
+  nombre_estado_solicitud VARCHAR(45) NOT NULL ,
+  descripcion_estado VARCHAR(255) NULL ,
+  PRIMARY KEY (id_estado_solicitud_req) );
+
+
+-- -----------------------------------------------------
+-- Table AREA
+-- -----------------------------------------------------
+CREATE TABLE AREA (
+  id_area SMALLINT NOT NULL ,
+  nombre_area VARCHAR(45) NOT NULL ,
+  descripcion_area TEXT NULL ,
+  PRIMARY KEY (id_area) );
+
+
+-- -----------------------------------------------------
+-- Table FUNCIONARIO
+-- -----------------------------------------------------
+CREATE TABLE FUNCIONARIO (
+  rut INT NOT NULL ,
+  nombre VARCHAR(45) NOT NULL ,
+  apellido_paterno VARCHAR(25) NOT NULL ,
+  apellido_materno VARCHAR(25) NOT NULL ,
+  correo_uv VARCHAR(45) NULL ,
+  fecha_ultimo_acceso TIMESTAMP NULL ,
+  fecha_primer_acceso TIMESTAMP NULL ,
+  PRIMARY KEY (rut) );
+
+
+-- -----------------------------------------------------
+-- Table FUNCIONARIO_DISICO
+-- -----------------------------------------------------
+CREATE TABLE FUNCIONARIO_DISICO (
+  rut INT NOT NULL ,
+  area SMALLINT NOT NULL ,
+  cargo VARCHAR(45) NOT NULL ,
+  anexo VARCHAR(5) NULL ,
+  PRIMARY KEY (rut) ,
+  CONSTRAINT fk_FUNCIONARIO_DISICO_FUNCIONARIOS1
+    FOREIGN KEY (rut )
+    REFERENCES FUNCIONARIO (rut )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_FUNCIONARIO_DISICO_AREA1
+    FOREIGN KEY (area )
+    REFERENCES AREA (id_area )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table TIPO_PRIORIDAD
+-- -----------------------------------------------------
+CREATE TABLE TIPO_PRIORIDAD (
+  id_tipo_prioridad SMALLINT NOT NULL ,
+  nombre_prioridad VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (id_tipo_prioridad) );
+
+
+-- -----------------------------------------------------
+-- Table SOLICITUD_REQUERIMIENTO
+-- -----------------------------------------------------
+CREATE TABLE SOLICITUD_REQUERIMIENTO (
+  id_solicitud_req BIGSERIAL NOT NULL ,
+  codigo_consulta VARCHAR(9) NOT NULL UNIQUE ,
+  asunto VARCHAR(45) NOT NULL ,
+  mensaje TEXT NOT NULL ,
+  tipo_solicitud SMALLINT NOT NULL ,
+  estado_solicitud SMALLINT NOT NULL ,
+  prioridad_solicitud SMALLINT NOT NULL ,
+  area_responsable SMALLINT NOT NULL ,
+  responsable INT NULL ,
+  solicitante INT NOT NULL ,
+  fecha_envio TIMESTAMP NOT NULL ,
+  fecha_cierre TIMESTAMP NULL ,
+  fecha_vencimiento TIMESTAMP NULL ,
+  fecha_ultima_actualizacion TIMESTAMP NOT NULL ,
+  justificacion_trasnferencia VARCHAR(255) NULL ,
+  respuesta TEXT NULL ,
+  PRIMARY KEY (id_solicitud_req) ,
+  CONSTRAINT fk_SOLICITUD_REQUERIMIENTO_TIPO_SOLICITUD_REQ
+    FOREIGN KEY (tipo_solicitud )
+    REFERENCES TIPO_SOLICITUD_REQ (id_tipo_solicitud_req )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_SOLICITUD_REQUERIMIENTO_ESTADO_SOLICITUD1
+    FOREIGN KEY (estado_solicitud )
+    REFERENCES ESTADO_SOLICITUD_REQ (id_estado_solicitud_req )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_SOLICITUD_REQUERIMIENTO_AREA1
+    FOREIGN KEY (area_responsable )
+    REFERENCES AREA (id_area )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_SOLICITUD_REQUERIMIENTO_FUNCIONARIO_DISICO1
+    FOREIGN KEY (responsable )
+    REFERENCES FUNCIONARIO_DISICO (rut )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_SOLICITUD_REQUERIMIENTO_FUNCIONARIOS1
+    FOREIGN KEY (solicitante )
+    REFERENCES FUNCIONARIO (rut )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_SOLICITUD_REQUERIMIENTO_TIPO_PRIORIDAD1
+    FOREIGN KEY (prioridad_solicitud )
+    REFERENCES TIPO_PRIORIDAD (id_tipo_prioridad )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table TIPO_PROYECTO
+-- -----------------------------------------------------
+CREATE TABLE TIPO_PROYECTO (
+  id_tipo_proyecto SMALLINT NOT NULL ,
+  nombre_tipo_proyecto VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (id_tipo_proyecto) );
+
+
+-- -----------------------------------------------------
+-- Table ESTADO_PROYECTO
+-- -----------------------------------------------------
+CREATE TABLE ESTADO_PROYECTO (
+  id_estado_proyecto SMALLINT NOT NULL ,
+  nombre_estado_proyecto VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (id_estado_proyecto) );
+
+
+-- -----------------------------------------------------
+-- Table PROYECTO
+-- -----------------------------------------------------
+CREATE TABLE PROYECTO (
+  id_proyecto SERIAL NOT NULL ,
+  codigo_interno VARCHAR(6) NOT NULL ,
+  nombre VARCHAR(45) NOT NULL ,
+  descripcion VARCHAR(255) NOT NULL ,
+  fecha_inicio TIMESTAMP NOT NULL ,
+  fecha_termino TIMESTAMP NULL ,
+  tipo_proyecto SMALLINT NOT NULL ,
+  estado_proyecto SMALLINT NOT NULL ,
+  PRIMARY KEY (id_proyecto) ,
+  CONSTRAINT fk_PROYECTO_TIPO_PROYECTO1
+    FOREIGN KEY (tipo_proyecto )
+    REFERENCES TIPO_PROYECTO (id_tipo_proyecto )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_PROYECTO_ESTADO_PROYECTO1
+    FOREIGN KEY (estado_proyecto )
+    REFERENCES ESTADO_PROYECTO (id_estado_proyecto )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table ESTADO_SOLICITUD_CAMBIO
+-- -----------------------------------------------------
+CREATE TABLE ESTADO_SOLICITUD_CAMBIO (
+  id_estado_solicitud_cambio SMALLINT NOT NULL ,
+  nombre_estado_solicitud VARCHAR(45) NOT NULL ,
+  descripcion_estado VARCHAR(255) NULL ,
+  PRIMARY KEY (id_estado_solicitud_cambio) )
+;
+
+
+-- -----------------------------------------------------
+-- Table ITEM_CONFIGURACION
+-- -----------------------------------------------------
+CREATE TABLE ITEM_CONFIGURACION (
+  id_item_configuracion SERIAL NOT NULL ,
+  codigo_identificador_ic VARCHAR(5) NOT NULL ,
+  nombre_item_configuracion VARCHAR(45) NOT NULL ,
+  version VARCHAR(10) NOT NULL ,
+  ubicacion_en_biblioteca VARCHAR(200) NOT NULL ,
+  fecha_ultima_modificacion TIMESTAMP NOT NULL ,
+  proyecto INT NOT NULL ,
+  responsable_item INT NOT NULL ,
+  PRIMARY KEY (id_item_configuracion) ,
+  CONSTRAINT fk_ITEM_CONFIGURACION_PROYECTO1
+    FOREIGN KEY (proyecto )
+    REFERENCES PROYECTO (id_proyecto )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_ITEM_CONFIGURACION_FUNCIONARIO_DISICO1
+    FOREIGN KEY (responsable_item )
+    REFERENCES FUNCIONARIO_DISICO (rut )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
+
+
+-- -----------------------------------------------------
+-- Table SOLICITUD_CAMBIO
+-- -----------------------------------------------------
+CREATE TABLE SOLICITUD_CAMBIO (
+  id_solicitud_cambio SERIAL NOT NULL ,
+  proyecto INT NOT NULL ,
+  solicitante INT NOT NULL ,
+  titulo VARCHAR(50) NOT NULL ,
+  fecha_envio TIMESTAMP NOT NULL ,
+  descripcion_necesidad_cambio TEXT NOT NULL ,
+  prioridad_solicitud SMALLINT NOT NULL ,
+  estado_solicitud SMALLINT NOT NULL ,
+  item_configuracion INT NOT NULL ,
+  descripcion_cambio TEXT NULL ,
+  evaluador_impacto INT NULL ,
+  fecha_analisis TIMESTAMP NULL ,
+  descripcion_impacto_cambio TEXT NULL ,
+  fecha_cierre TIMESTAMP NULL ,
+  modulo_afectado VARCHAR(60) NULL ,
+  evaluador_final INT NULL ,
+  descripcion_resolucion VARCHAR(255) NULL ,
+  aprobada BOOLEAN NULL ,
+  PRIMARY KEY (id_solicitud_cambio) ,
+  CONSTRAINT fk_SOLICITUD_CAMBIO_PROYECTO1
+    FOREIGN KEY (proyecto )
+    REFERENCES PROYECTO (id_proyecto )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_SOLICITUD_CAMBIO_FUNCIONARIO_DISICO1
+    FOREIGN KEY (solicitante )
+    REFERENCES FUNCIONARIO_DISICO (rut )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_SOLICITUD_CAMBIO_TIPO_PRIORIDAD1
+    FOREIGN KEY (prioridad_solicitud )
+    REFERENCES TIPO_PRIORIDAD (id_tipo_prioridad )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_SOLICITUD_CAMBIO_ESTADO_SOLICITUD_CAMBIO1
+    FOREIGN KEY (estado_solicitud )
+    REFERENCES ESTADO_SOLICITUD_CAMBIO (id_estado_solicitud_cambio )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_SOLICITUD_CAMBIO_FUNCIONARIO_DISICO2
+    FOREIGN KEY (evaluador_impacto )
+    REFERENCES FUNCIONARIO_DISICO (rut )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_SOLICITUD_CAMBIO_FUNCIONARIO_DISICO3
+    FOREIGN KEY (evaluador_final )
+    REFERENCES FUNCIONARIO_DISICO (rut )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_SOLICITUD_CAMBIO_ITEM_CONFIGURACION1
+    FOREIGN KEY (item_configuracion )
+    REFERENCES ITEM_CONFIGURACION (id_item_configuracion )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table TAREA_SCM
+-- -----------------------------------------------------
+CREATE TABLE TAREA_SCM (
+  id_tarea_scm SERIAL NOT NULL ,
+  nombre_tarea VARCHAR(60) NOT NULL ,
+  descripcion_tarea VARCHAR(255) NOT NULL ,
+  PRIMARY KEY (id_tarea_scm) );
+
+
+-- -----------------------------------------------------
+-- Table NOTIFICACION
+-- -----------------------------------------------------
+CREATE TABLE NOTIFICACION (
+  id_notificacion BIGSERIAL NOT NULL ,
+  fecha TIMESTAMP NOT NULL ,
+  mensaje_notificacion VARCHAR(180) NOT NULL ,
+  revisada BOOLEAN NOT NULL DEFAULT false ,
+  destinatario INT NOT NULL ,
+  PRIMARY KEY (id_notificacion) ,
+  CONSTRAINT fk_NOTIFICACION_FUNCIONARIO1
+    FOREIGN KEY (destinatario )
+    REFERENCES FUNCIONARIO (rut )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table TAREA_PROYECTO
+-- -----------------------------------------------------
+CREATE TABLE TAREA_PROYECTO (
+  id_tarea_proyecto SERIAL NOT NULL,
+  proyecto INT NOT NULL ,
+  responsable_tarea INT NOT NULL ,
+  tarea VARCHAR(60) NOT NULL ,
+  descripcion_tarea VARCHAR(255) NOT NULL ,
+  fecha_creacion DATE NOT NULL ,
+  fecha_inicio_propuesta TIMESTAMP NOT NULL ,
+  fecha_inicio_real TIMESTAMP NULL ,
+  fecha_termino_propuesta TIMESTAMP NOT NULL ,
+  fecha_termino_real TIMESTAMP NULL ,
+  nivel_avance SMALLINT NOT NULL ,
+  visible BOOLEAN NOT NULL ,
+  PRIMARY KEY (id_tarea_proyecto) ,
+  CONSTRAINT fk_TAREA_PROYECTO_PROYECTO1
+    FOREIGN KEY (proyecto )
+    REFERENCES PROYECTO (id_proyecto )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_TAREA_PROYECTO_FUNCIONARIO_DISICO1
+    FOREIGN KEY (responsable_tarea )
+    REFERENCES FUNCIONARIO_DISICO (rut )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table COMENTARIO_SOLICITUD
+-- -----------------------------------------------------
+CREATE TABLE COMENTARIO_SOLICITUD (
+  id_comentario BIGSERIAL NOT NULL,
+  solicitud_requerimiento BIGINT NOT NULL ,
+  autor INT NOT NULL ,
+  comentario TEXT NOT NULL ,
+  fecha TIMESTAMP NOT NULL ,
+  visible BOOLEAN NOT NULL DEFAULT true ,
+  PRIMARY KEY (id_comentario) ,
+  CONSTRAINT fk_COMENTARIO_SOLICITUD_SOLICITUD_REQUERIMIENTO1
+    FOREIGN KEY (solicitud_requerimiento )
+    REFERENCES SOLICITUD_REQUERIMIENTO (id_solicitud_req )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_COMENTARIO_SOLICITUD_FUNCIONARIOS1
+    FOREIGN KEY (autor )
+    REFERENCES FUNCIONARIO (rut )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table TAREA_SCM_PROYECTO
+-- -----------------------------------------------------
+CREATE TABLE TAREA_SCM_PROYECTO (
+  id_tarea_scm INT NOT NULL ,
+  id_proyecto INT NOT NULL ,
+  responsable INT NOT NULL ,
+  fecha_inicio DATE NOT NULL ,
+  fecha_termino DATE NOT NULL ,
+  PRIMARY KEY (id_tarea_scm, id_proyecto) ,
+  CONSTRAINT fk_TAREA_SCM_has_PROYECTO_TAREA_SCM1
+    FOREIGN KEY (id_tarea_scm )
+    REFERENCES TAREA_SCM (id_tarea_scm )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_TAREA_SCM_has_PROYECTO_PROYECTO1
+    FOREIGN KEY (id_proyecto )
+    REFERENCES PROYECTO (id_proyecto )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_TAREA_SCM_has_PROYECTO_FUNCIONARIO_DISICO1
+    FOREIGN KEY (responsable )
+    REFERENCES FUNCIONARIO_DISICO (rut )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table ENTREGABLE
+-- -----------------------------------------------------
+CREATE TABLE ENTREGABLE (
+  id_entregable SERIAL NOT NULL ,
+  nombre_entregable VARCHAR(50) NOT NULL ,
+  tarea_scm INT NOT NULL ,
+  PRIMARY KEY (id_entregable) ,
+  CONSTRAINT fk_ENTREGABLE_TAREA_SCM1
+    FOREIGN KEY (tarea_scm )
+    REFERENCES TAREA_SCM (id_tarea_scm )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table ROL_PROYECTO
+-- -----------------------------------------------------
+CREATE TABLE ROL_PROYECTO (
+  id_rol SERIAL NOT NULL ,
+  nombre_rol VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (id_rol) )
+;
+
+
+-- -----------------------------------------------------
+-- Table PARTICIPANTE_PROYECTO
+-- -----------------------------------------------------
+CREATE TABLE PARTICIPANTE_PROYECTO (
+  rut_participante INT NOT NULL ,
+  id_proyecto INT NOT NULL ,
+  rol INT NOT NULL ,
+  PRIMARY KEY (rut_participante, id_proyecto) ,
+  CONSTRAINT fk_FUNCIONARIO_DISICO_has_PROYECTO_FUNCIONARIO_DISICO1
+    FOREIGN KEY (rut_participante )
+    REFERENCES FUNCIONARIO_DISICO (rut )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_FUNCIONARIO_DISICO_has_PROYECTO_PROYECTO1
+    FOREIGN KEY (id_proyecto )
+    REFERENCES PROYECTO (id_proyecto )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_PARTICIPANTE_PROYECTO_ROL_PROYECTO1
+    FOREIGN KEY (rol )
+    REFERENCES ROL_PROYECTO (id_rol )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table FORMULARIO_IMPLEMENTACION
+-- -----------------------------------------------------
+CREATE TABLE FORMULARIO_IMPLEMENTACION (
+  id_formulario_implementacion INT NOT NULL ,
+  observaciones TEXT NOT NULL ,
+  fecha_verificacion DATE NOT NULL ,
+  verificador INT NOT NULL ,
+  implementador INT NOT NULL ,
+  nueva_version VARCHAR(10) NOT NULL ,
+  nuevo_identificador VARCHAR(5) NOT NULL ,
+  PRIMARY KEY (id_formulario_implementacion) ,
+  CONSTRAINT fk_FORMULARIO_IMPLEMENTACION_FUNCIONARIO_DISICO1
+    FOREIGN KEY (verificador )
+    REFERENCES FUNCIONARIO_DISICO (rut )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_FORMULARIO_IMPLEMENTACION_FUNCIONARIO_DISICO2
+    FOREIGN KEY (implementador )
+    REFERENCES FUNCIONARIO_DISICO (rut )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_FORMULARIO_IMPLEMENTACION_SOLICITUD_CAMBIO1
+    FOREIGN KEY (id_formulario_implementacion )
+    REFERENCES SOLICITUD_CAMBIO (id_solicitud_cambio )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table ESTADISTICA_PERSONAL
+-- -----------------------------------------------------
+CREATE TABLE ESTADISTICA_PERSONAL (
+  id BIGSERIAL NOT NULL ,
+  funcionario INT NOT NULL ,
+  fecha_medicion DATE NOT NULL ,
+  cantidad_total_solicitudes_asignadas BIGINT NOT NULL ,
+  cantidad_solicitudes_pendientes INT NOT NULL ,
+  cantidad_solicitudes_vencidas INT NOT NULL ,
+  cantidad_solicitudes_iniciadas INT NOT NULL ,
+  cantidad_solicitudes_cerradas INT NOT NULL ,
+  cantidad_proyectos_acargo INT NOT NULL ,
+  cantidad_proyectos_en_que_participa INT NOT NULL ,
+  cantidad_tareas_proyecto_asociadas INT NOT NULL ,
+  cantidad_tareas_scm_asociadas INT NOT NULL ,
+  PRIMARY KEY (id) ,
+  CONSTRAINT fk_ESTADISTICA_PERSONAL_FUNCIONARIO_DISICO1
+    FOREIGN KEY (funcionario )
+    REFERENCES FUNCIONARIO_DISICO (rut )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
