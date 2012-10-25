97a7f89b1fea440090193a0be3d835393decc981
Alejandro Alvarez <yano2h@gmail.com>
2012-10-25 17:26:30 -0300

    Modificacion envio de correos

--- CodigoFuente/Proyecto/Proyecto-ejb/src/java/cl/uv/proyecto/mensajeria/ejb/EmailEJB.java
+++ CodigoFuente/Proyecto/Proyecto-ejb/src/java/cl/uv/proyecto/mensajeria/ejb/EmailEJB.java
@@ -4,6 +4,9 @@
  */
 package cl.uv.proyecto.mensajeria.ejb;
 
+import cl.uv.model.base.utils.Resources;
+import cl.uv.proyecto.persistencia.entidades.Funcionario;
+import cl.uv.proyecto.persistencia.entidades.SolicitudRequerimiento;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,10 +19,14 @@ import javax.mail.internet.MimeMessage;
 
 @Stateless
 public class EmailEJB implements EmailEJBLocal {
-
+ 
     @Resource(name = "email/ssrscm2")
     private Session mailSession;
-
+    
+    private String propertieEmail = "Emails";
+    private String prefijoAsunto = "ASUNTO_";
+    private String prefijoMensaje = "MSG_";
+    
     @Override
     @Asynchronous
     public void enviarEmail(String direccion, String asunto, String mensaje) {
@@ -60,8 +67,76 @@ public class EmailEJB implements EmailEJBLocal {
         }
     }
     
-    public void hacerNanda(){
-        System.out.println("NADAAA");
+    @Override
+    public String generarContenidoEmailSolicitud(TypeEmail t, SolicitudRequerimiento s, Funcionario invoker){
+        String msg = Resources.getValue(propertieEmail, prefijoMensaje+t.name());
+        switch(t){
+            case ENVIO_SOLICITUD:
+                msg = String.format(msg,s.getAsunto(),s.getCodigoConsulta(),"link");
+                break;
+            case RECHAZO_SOLICITUD:
+
+                break;
+            case TRANSFERENCIA_SOLICITUD:
+
+                break;
+            case ASIGNACION_SOLICITUD:
+
+                break;
+            case INICIO_SOLICITUD:
+
+                break;
+            case CIERRE_SOLICITD:
+
+                break;
+            case COMENTARIO_SOLICITUD:
+
+                break;
+            default:
+                throw new AssertionError("Tipo enum desconocido");
+        }
+                
+                
+        return msg;
+    }
+    
+    @Override
+    public String generarAsuntoEmailSolicitud(TypeEmail t, SolicitudRequerimiento s, Funcionario invoker){
+        String asunto = Resources.getValue(propertieEmail, prefijoAsunto+t.name());
+        
+        switch(t){
+            case ENVIO_SOLICITUD:
+                asunto = String.format(asunto,s.getAsunto());
+                break;
+            case RECHAZO_SOLICITUD:
+
+                break;
+            case TRANSFERENCIA_SOLICITUD:
+
+                break;
+            case ASIGNACION_SOLICITUD:
+
+                break;
+            case INICIO_SOLICITUD:
+
+                break;
+            case CIERRE_SOLICITD:
+
+                break;
+            case COMENTARIO_SOLICITUD:
+
+                break;
+            default:
+                throw new AssertionError("Tipo enum desconocido");
+        }
+        
+        return asunto;
     }
    
+    @Override
+    public void enviarEmailConfirmacionEnvioSolicitud(SolicitudRequerimiento s){
+        String asunto = generarAsuntoEmailSolicitud(TypeEmail.ENVIO_SOLICITUD, s, s.getSolicitante());
+        String msg = generarContenidoEmailSolicitud(TypeEmail.ENVIO_SOLICITUD, s, s.getSolicitante());
+        enviarEmail(s.getSolicitante().getCorreoUv(), asunto, msg);
+    }
 }
