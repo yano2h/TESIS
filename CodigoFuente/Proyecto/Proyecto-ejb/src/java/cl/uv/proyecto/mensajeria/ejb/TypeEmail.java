72bcc5ce57166ff109f5f71ea31ed08f9f9b0f3c
Alejandro Alvarez <yano2h@gmail.com>
2012-10-25 17:27:45 -0300

    enum tipo email

---
+++ CodigoFuente/Proyecto/Proyecto-ejb/src/java/cl/uv/proyecto/mensajeria/ejb/TypeEmail.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.uv.proyecto.mensajeria.ejb;
+
+/**
+ *
+ * @author Alejandro
+ */
+public enum TypeEmail {
+    ENVIO_SOLICITUD,
+    RECHAZO_SOLICITUD,
+    TRANSFERENCIA_SOLICITUD,
+    ASIGNACION_SOLICITUD,
+    INICIO_SOLICITUD,
+    CIERRE_SOLICITD,
+    COMENTARIO_SOLICITUD
+}
